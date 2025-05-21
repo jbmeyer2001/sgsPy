6a12b80603af684d45bc4ac71b875d7eb76b9d77
jbmeyer2001 <jbmeyerii@gmail.com>
2025-05-21 11:37:30 -0700

    barebones raster data structure

--- sgs/utils/raster.py
+++ sgs/utils/raster.py
@@ -1,3 +1,5 @@
+import copy
+import json
 import os
 
 from osgeo import gdal, osr
@@ -6,13 +8,10 @@ import matplotlib.pyplot as plt
 
 class SpatialRaster:
     def __init__(self, image):
-        # TODO add other input types as required
         if type(image) == str:
             self.dataset = gdal.Open(image, gdal.GA_ReadOnly)
         elif type(image) == gdal.Dataset:
             self.dataset = image
-        elif type(image) == SpatialRaster:
-            self.dataset = image.dataset
         else:
             raise TypeError(f"SpatialRaster does not except input of type {type(image)}")
 
@@ -23,7 +22,7 @@ class SpatialRaster:
         self.width = self.dataset.RasterXSize
         self.height = self.dataset.RasterYSize
         self.layers = self.dataset.RasterCount
-        self.crs = osr.SpatialReference(wkt=self.dataset.GetProjection())
+        self.crs = json.loads(osr.SpatialReference(wkt=self.dataset.GetProjection()).ExportToPROJJSON())
         self.geotransform = self.dataset.GetGeoTransform()
         if self.geotransform:
             xbounds = [self.geotransform[0], self.geotransform[0] + self.geotransform[1] * self.width + self.geotransform[2] * self.height]
@@ -32,23 +31,24 @@ class SpatialRaster:
             self.xmax = np.max(xbounds)
             self.ymin = np.min(ybounds)
             self.ymax = np.max(ybounds)
+            self.pixel_height = np.abs(self.geotransform[1])
+            self.pixel_width = np.abs(self.geotransform[5])
         self.arr = None
         self.band_name_dict = {}
         for i in range(0, self.layers): #raster bands are 1 indexed
             self.band_name_dict[self.dataset.GetRasterBand(i + 1).GetDescription()] = i
 
-
     def info(self):
         print("driver: {}".format(self.driver))
         print("size: {} x {} x {}".format(self.width, self.height, self.layers))
         if self.geotransform:
-            print("pixel size: (x, y): ({}, {})".format(np.abs(self.geotransform[1]), np.abs(self.geotransform[5])))
+            print("pixel size: (x, y): ({}, {})".format(self.pixel_height, self.pixel_width))
             print("bounds (xmin, xmax, ymin, ymax): ({}, {}, {}, {})".format(self.xmin, self.xmax, self.ymin, self.ymax))
 
     # NOTE -- this is the most naive implementation possible. There is no manipulation
     # of tiles, and no specification of how much memory to allocate, etc.
     #
-    # TODO in the future, for more intensive tasks, this may have to be changed.
+    # in the future, for more intensive tasks, this may have to be changed.
     def load_arr(self):
         self.arr = self.dataset.GetVirtualMemArray()
 
@@ -69,16 +69,26 @@ class SpatialRaster:
         return self.arr[index]
 
     def arrange_bands_from_list(bands_list):
-        #TODO implement this
-        #TODO check to ensure alpha isn't included because I won't use it
-        pass
+        num_items = len(bands_list)
+        if num_items != 1 and num_items != 3:
+            raise ValueError("number of bands must be either 1 (for scalar iamges) or 3 (for RGB images).")
+
+        for i in range(num_items):
+            bands_list[i] = self.get_band_index(bands_list[i])
+
+        return bands_list
 
     def arrange_bands_from_dict(bands_dict):
-        #TODO implement this
-        #only accept lists of length 1 or 3
-        pass
+        if len(bands_dict) != 3 or 'red' not in bands_dict or 'green' not in bands_dict or 'blue' not in bands_dict:
+            raise ValueError("if bands is a dict, it must to have three items with the keys 'red', 'green', and 'blue'.")
 
-    def plot_image(self, bands=None, **kwargs):
+        return [
+            self.get_band_index(bands_dict["red"]),
+            self.get_band_index(bands_dict["green"]),
+            self.get_band_index(bands_dict["blue"])
+        ]
+
+    def plot_image(self, bands=None, fname=None, show=True, **kwargs):
         if bands is None:
             bands = self.arrange_bands_from_list([*range(self.layers)])
         elif type(bands) == list:
@@ -88,12 +98,14 @@ class SpatialRaster:
         elif type(bands) in [str, int]: 
             bands = [self.get_band_index(bands)]
         else:
-            pass
-            #TODO raise value error
+            raise TypeError("'bands' parameter must be of type None, list, dict, str, or int.")
 
         if self.arr is None:
             self.load_arr()
 
         display_arr = np.moveaxis(self.arr[bands, :, :], 0, 2)
         plt.imshow(display_arr, **kwargs)
-        plt.show()
+        if fname is not None:
+            plt.savefig(fname)
+        if show:
+            plt.show()
--- tests/utils/test_raster.py
+++ tests/utils/test_raster.py
@@ -2,13 +2,71 @@ import pytest
 import sgs
 from osgeo import gdal
 
-mraster_small_path = '/home/jbmeyer/extdata/mraster.tif'
+mraster_path = '/home/jbmeyer/extdata/mraster.tif'
+mraster_small_path = '/home/jbmeyer/extdata/mraster_small.tif'
+sraster_path = '/home/jbmeyer/extdata/sraster.tif'
 
 class TestSpatialRaster:
+    def mraster_parameters_check(self, rast):
+        assert rast.width == 373
+        assert rast.height == 277
+        assert rast.layers == 3
+        assert rast.pixel_width == 20
+        assert rast.pixel_height == 20
+        assert rast.xmin == 431100
+        assert rast.xmax == 438560
+        assert rast.ymin == 5337700
+        assert rast.ymax == 5343240
+
+    def mraster_small_parameters_check(self, rast):
+        assert rast.width == 141
+        assert rast.height == 110
+        assert rast.layers == 3
+        assert rast.pixel_width == 20
+        assert rast.pixel_height == 20
+        assert rast.xmin == 432440
+        assert rast.xmax == 435260
+        assert rast.ymin == 5340000
+        assert rast.ymax == 5342200
+    
+    def sraster_parameters_check(self, rast):
+        assert rast.width == 373
+        assert rast.height == 277
+        assert rast.layers == 1
+        assert rast.pixel_width == 20
+        assert rast.pixel_height == 20
+        assert rast.xmin == 431100
+        assert rast.xmax == 438560
+        assert rast.ymin == 5337700
+        assert rast.ymax == 5343240
+
     def test_construct_from_path(self):
+        rast = sgs.utils.raster.SpatialRaster(mraster_path)
+        self.mraster_parameters_check(rast)
+
         rast = sgs.utils.raster.SpatialRaster(mraster_small_path)
+        self.mraster_small_parameters_check(rast)
+
+        rast = sgs.utils.raster.SpatialRaster(sraster_path)
+        self.sraster_parameters_check(rast)
 
     def test_construct_from_gdal_dataset(self):
+        dataset = gdal.Open(mraster_path, gdal.GA_ReadOnly)
+        rast = sgs.utils.raster.SpatialRaster(dataset)
+        self.mraster_parameters_check(rast)
+
         dataset = gdal.Open(mraster_small_path, gdal.GA_ReadOnly)
         rast = sgs.utils.raster.SpatialRaster(dataset)
+        self.mraster_small_parameters_check(rast)
 
+        dataset = gdal.Open(sraster_path, gdal.GA_ReadOnly)
+        rast = sgs.utils.raster.SpatialRaster(dataset)
+        self.sraster_parameters_check(rast)
+
+'''
+TODO: 
+ - use test image that has different pixel_width and pixel_height values
+ - test the actual pixel values within the image
+ - test different image input types
+ - test image plotting?
+'''
