e55eb4e265789026e0b7b928f478f6d26127ae7b
jbmeyer2001 <jbmeyerii@gmail.com>
2025-05-27 07:11:17 -0700

    add cpp code from BalancedSampling R package

---
+++ sgs/sample/balanced/BalancedSampling/src/ReducedRowEchelonForm.h
@@ -0,0 +1,13 @@
+#ifndef REDUCEDROWECHELONFORM_HEADER
+#define REDUCEDROWECHELONFORM_HEADER
+
+#include <stddef.h>
+
+void ReducedRowEchelonForm(
+  double *mat,
+  const size_t nrows,
+  const size_t ncolumns
+);
+
+#endif
+
