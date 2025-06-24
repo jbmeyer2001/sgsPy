74f5b5ec1c137a4d4b5e9bcaccc37aac8a2c06ef
jbmeyer2001 <jbmeyerii@gmail.com>
2025-06-24 07:27:24 -0700

    remove test_write.py

--- tests/utils/test_write.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import sgs.utils.write as write
-
-class TestWrite:
-    #TODO add real tests before implementing write
-    @pytest.mark.xfail
-    def test_not_implemented(self):
-        write()
