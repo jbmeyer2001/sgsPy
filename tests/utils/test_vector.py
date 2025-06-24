fb40fa16c2b7bd8a296ed5e8e3abcbf665841754
jbmeyer2001 <jbmeyerii@gmail.com>
2025-06-24 08:21:26 -0700

    test vector metadata

--- tests/utils/test_vector.py
+++ tests/utils/test_vector.py
@@ -11,39 +11,41 @@ from files import (
 
 class TestSpatialVector:
     def access_parameters_check(self, vec):
-        info = vec.layers_info[0]
-        assert info['name'] == 'access'
-        assert info['features'] == 167
-        assert info['fields'] == 2
-        (xmin, xmax, ymin, ymax) = info['extent']
-        assert xmin == 431100
-        assert xmax == 438560
-        assert ymin == 5337700
-        assert ymax == 5343240
+        assert len(vec.layers) == 1
+        assert vec.layers[0] == 'access'
+        layer_info = vec.cpp_vector.get_layer_info('access')
+        assert int(layer_info["feature_count"]) == 167
+        assert int(layer_info["field_count"]) == 2
+        assert layer_info["geometry_type"] == "Line String"
+        assert float(layer_info["xmin"]) == 431100
+        assert float(layer_info["xmax"]) == 438560
+        assert float(layer_info["ymin"]) == 5337700
+        assert float(layer_info["ymax"]) == 5343240
 
     def existing_parameters_check(self, vec):
-        info = vec.layers_info[0]
-        assert info['name'] == 'existing'
-        assert info['features'] == 200
-        assert info['fields'] == 1
-        (xmin, xmax, ymin, ymax) = info['extent']
-        assert xmin == 431110
-        assert xmax == 438530
-        assert ymin == 5337710
-        assert ymax == 5343230
+        assert len(vec.layers) == 1
+        assert vec.layers[0] == 'existing'
+        layer_info = vec.cpp_vector.get_layer_info('existing')
+        assert int(layer_info["feature_count"]) == 200
+        assert int(layer_info["field_count"]) == 1
+        assert layer_info["geometry_type"] == "Point"
+        assert float(layer_info["xmin"]) == 431110
+        assert float(layer_info["xmax"]) == 438530
+        assert float(layer_info["ymin"]) == 5337710
+        assert float(layer_info["ymax"]) == 5343230
 
     def inventory_polygons_parameters_check(self, vec):
-        info = vec.layers_info[0]
-        assert info['name'] == 'inventory_polygons'
-        assert info['features'] == 632
-        assert info['fields'] == 3
-        (xmin, xmax, ymin, ymax) = info['extent']
-        assert xmin == 431100
-        assert xmax == 438560
-        assert ymin == 5337700
-        assert ymax == 5343240
+        assert len(vec.layers) == 1
+        assert vec.layers[0] == 'inventory_polygons'
+        layer_info = vec.cpp_vector.get_layer_info('inventory_polygons')
+        assert int(layer_info["feature_count"]) == 632
+        assert int(layer_info["field_count"]) == 3
+        assert layer_info["geometry_type"] == "Polygon"
+        assert float(layer_info["xmin"]) == 431100
+        assert float(layer_info["xmax"]) == 438560
+        assert float(layer_info["ymin"]) == 5337700
+        assert float(layer_info["ymax"]) == 5343240
 
-    @pytest.mark.skip(reason="C++ vector functionality not implemented yet")
     def test_construct_from_path(self):
         vec = sgs.utils.vector.SpatialVector(access_shapefile_path)
         self.access_parameters_check(vec)
@@ -54,10 +56,6 @@ class TestSpatialVector:
         vec = sgs.utils.vector.SpatialVector(inventory_polygons_shapefile_path)
         self.inventory_polygons_parameters_check(vec)
 
-    @pytest.mark.skip(reason="C++ vector functionality not implemented yet")
-    def test_common_file_formats(self):
-        vec = sgs.utils.vector.SpatialVector(existing_geodatabase_path)
-        self.existing_parameters_check(vec)
-
+    def test_geojson_format(self):
         vec = sgs.utils.vector.SpatialVector(existing_geojson_path)
         self.existing_parameters_check(vec)
