09d266fa2983ef057a2c6b8511c4e3c3660d59dd
jbmeyer2001 <jbmeyerii@gmail.com>
2025-11-13 12:00:06 -0800

    test on large raster

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import sgs
 
-rast = sgs.SpatialRaster('/home/jbmeyer/extdata/mraster.tif')
-pcomp = sgs.calculate.pca(rast, 3, filename='out/new_pcomp_result.tif')
-pcomp.plot(band=0)
-pcomp.plot(band=1)
-pcomp.plot(band=2)
+rast = sgs.SpatialRaster('/home/jbmeyer/RMF_LiDAR_Metrics/merged.tif')
+pcomp = sgs.calculate.pca(rast, 3, filename="large_pca_out.tif")
+#pcomp.plot(band=0)
+#pcomp.plot(band=1)
+#pcomp.plot(band=2)
 
 #rast = sgs.SpatialRaster('/home/jbmeyer/RMF_LiDAR_Metrics/Merged_Aspect_DEM5m.tif')
 #pcomp = sgs.calculate.pca(rast, 1, filename='aspect_dem_pca.tif')
