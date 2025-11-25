c52c3e4b11143fc29d474605c83669201a4c4732
jbmeyer2001 <jbmeyerii@gmail.com>
2025-11-25 10:17:11 -0800

    WIP linux

--- build.bat
+++ build.bat
@@ -10,7 +10,7 @@ cd ../../..
 
 :: edit the gdal pkgconfig file to say 'gdal.lib' instead of 'gdal'
 :: this is very hacky but the build system searches for a nonexistant gdal.obj file if not done
-Powershell -Command "(gc C:\Users\jmeyer03\projects\Github\sgs\sgs\extern\vcpkg\installed\x64-windows\lib\pkgconfig\gdal.pc) -replace 'lib gdal', 'lib  gdal.lib' | Out-File -encoding ASCII C:\Users\jmeyer03\projects\Github\sgs\sgs\extern\vcpkg\installed\x64-windows\lib\pkgconfig\gdal.pc"
+Powershell -Command "(gc %CD%\sgs\extern\vcpkg\installed\x64-windows\lib\pkgconfig\gdal.pc) -replace 'lib gdal', 'lib  gdal.lib' | Out-File -encoding ASCII %CD%\sgs\extern\vcpkg\installed\x64-windows\lib\pkgconfig\gdal.pc"
 
 :: set environment variables
 set "PKG_CONFIG=%CD%\sgs\extern\vcpkg\installed\x64-windows\tools\pkgconf\pkgconf.exe"
