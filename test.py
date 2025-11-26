53cc039ed690f57d838c72d4f53384778d604984
jbmeyer2001 <jbmeyerii@gmail.com>
2025-11-26 07:55:30 -0800

    pytests passing linux

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import sgs
 
-#print("HERE 1")
-#rast = sgs.SpatialRaster('/home/jbmeyer/RMF_LiDAR_Metrics/merged.tif')
-#print("HERE 2")
-#srast = sgs.stratify.quantiles(rast, num_strata={rast.bands[0]:10}, filename="large_quantiles.tif")
-#print("HERE 3")
-#srast.plot(band=0)
+print("HERE 1")
+rast = sgs.SpatialRaster('/home/jbmeyer/extdata/mraster.tif')
+print("HERE 2")
+srast = sgs.stratify.quantiles(rast, num_strata={rast.bands[0]:10}, filename="test.tif")
+print("HERE 3")
+srast.plot(band=0)
 
-rast = sgs.SpatialRaster('/home/jbmeyer/RMF_LiDAR_Metrics/Merged_Aspect_DEM5m.tif')
-pcomp = sgs.calculate.pca(rast, 1, filename='aspect_dem_pca.tif')
+#rast = sgs.SpatialRaster('/home/jbmeyer/RMF_LiDAR_Metrics/Merged_Aspect_DEM5m.tif')
+#pcomp = sgs.calculate.pca(rast, 1, filename='aspect_dem_pca.tif')
 
