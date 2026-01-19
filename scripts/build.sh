e119017af76f0557a7ecd27323ad003d69a5dd23
jbmeyer2001 <jbmeyerii@gmail.com>
2026-01-19 15:58:04 -0800

    try fix gh actions

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #set pkgconfig environment variables
-export PKG_CONFIG="${PWD}/sgspy/extern/vcpkg/installed/x64-linux/tools/pkgconf/pkgconf"
-export PKG_CONFIG_PATH="${PWD}/sgspy/extern/vcpkg/installed/x64-linux/lib/pkgconfig"
+export PKG_CONFIG="${PWD}/sgspy/extern/vcpkg/installed/x64-linux-release/tools/pkgconf/pkgconf"
+export PKG_CONFIG_PATH="${PWD}/sgspy/extern/vcpkg/installed/x64-linux-release/lib/pkgconfig"
 
 #run build command
 clear
