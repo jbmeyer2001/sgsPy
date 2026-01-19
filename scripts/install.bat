608de3f08590317c2be7d2af83f3f9e5473f053d
jbmeyer2001 <jbmeyerii@gmail.com>
2026-01-19 14:41:58 -0800

    only build release versions

--- scripts/install.bat
+++ scripts/install.bat
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 :: run vcpkg initialization commands
 cd sgspy\extern\vcpkg
 Powershell -Command .\bootstrap-vcpkg
-.\vcpkg install boost-asio
-.\vcpkg install gdal
+.\vcpkg install boost-asio:x64-windows-release
+.\vcpkg install gdal:x64-windows-release
 cd ../../..
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive
 #run vcpkg intialization commands
 cd sgspy/extern/vcpkg
 ./bootstrap-vcpkg.sh
-./vcpkg install boost-asio
-./vcpkg install gdal
+./vcpkg install boost-asio:x64-linux-release
+./vcpkg install gdal:x64-linux-release
 cd ../../..
 
