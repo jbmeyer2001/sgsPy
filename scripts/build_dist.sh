fa90f57f3732d3bcdd616f0020498ffa53afa3c2
jbmeyer2001 <jbmeyerii@gmail.com>
2026-01-26 12:25:34 -0800

    update test

--- scripts/build_dist.sh
+++ scripts/build_dist.sh
@@ -9,6 +9,3 @@ export PKG_CONFIG_PATH="${PWD}/sgspy/extern/vcpkg/installed/x64-linux/lib/pkgcon
 clear
 python -m build --wheel --outdir dist 
 
-#run auditwheel on wheel to ensure manylinux compliance, excluding libraries which will come from a Python dependency
-auditwheel repair dist/*.whl -w dist --plat manylinux_2_35_x86_64 --exclude libmkl_core.so.2 --exclude libmkl_tbb_thread.so.2 --exclude libmkl_intel_ilp64.so.2 --exclude libonedal_parameters.so.3 --exclude libonedal_core.so.3 --exclude libonedal_thread.so.3 --exclude libonedal.so.3
-rm dist/*linux_x86_64.whl
