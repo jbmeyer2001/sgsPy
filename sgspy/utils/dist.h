dc07c9c3b46d92c81b9688f2aedc07d271023e76
jbmeyer2001 <jbmeyerii@gmail.com>
2026-01-30 13:38:12 -0800

    compiles

--- sgspy/utils/dist.h
+++ sgspy/utils/dist.h
@@ -24,8 +24,9 @@ namespace sgs {
 namespace dist {
 
 //no threading to start out
-template typename<T>
-findMinMax<T>(
+template <typename T>
+void 
+findMinMax(
 	helper::RasterBandMetaData& band, 
 	int width, 
 	int yStartBlock,
@@ -38,7 +39,7 @@ findMinMax<T>(
 	min = std::numeric_limits<T>::max();
 	max = std::numeric_limits<T>::min();
 	T nan = static_cast<T>(band.nan);
-	p_data = VSIMalloc3(xBlockSize, yBlockSize, band.size);
+	void *p_data = VSIMalloc3(band.xBlockSize, band.yBlockSize, band.size);
 
 	//calculate raster band minimum and maximum values
 	for (int yBlock = yStartBlock; yBlock < yEndBlock; yBlock++) {
@@ -52,10 +53,10 @@ findMinMax<T>(
 			for (int y = 0; y < yValid; y++) {
 				int index = y * width;
 				for (int x = 0; x < xValid; x++) {
-					T val = p_data[index];
+					T val = reinterpret_cast<T *>(p_data)[index];
 					if (val != nan && !std::isnan(val)) {
-						tmin = std::min(tmin, val);
-						tmax = std::max(tmax, val);
+						min = std::min(min, val);
+						max = std::max(max, val);
 					}
 					index++;
 				}
@@ -91,7 +92,7 @@ setBuckets(
 		}
 	}
 	else {
-		retval[0] = tmin;
+		retval[0] = min;
 		cur += step;
 		for (int i = 1; i < nBuckets; i++) {
 			retval[i] = std::ceil(cur);
@@ -116,7 +117,7 @@ populationDistribution(
 	int xBlocks = (width + band.xBlockSize - 1) / band.xBlockSize;
 
 	T nan = static_cast<T>(band.nan);
-	T *p_data = VSIMalloc3(xBlockSize, YBlockSize, band.size);
+	void *p_data = VSIMalloc3(band.xBlockSize, band.yBlockSize, band.size);
 
 	for (int yBlock = yBlockStart; yBlock < yBlockEnd; yBlock++) {
 		for (int xBlock = 0; xBlock < xBlocks; xBlock++) {
@@ -129,7 +130,7 @@ populationDistribution(
 			for (int y = 0; y < yValid; y++) {
 				int index = y * width;
 				for (int x = 0; x < xValid; x++) {
-					T val = p_data[index];
+					T val = reinterpret_cast<T *>(p_data)[index];
 					
 					if (val != nan && !std::isnan(val)) {
 						for (int i = nBuckets - 1; i >= 0; i--) {
@@ -151,14 +152,14 @@ template <typename T>
 std::vector<int64_t>
 sampleDistribution(
 	helper::RasterBandMetaData& band,
-	std::vector<Index>& samples,
+	std::vector<helper::Index>& samples,
 	std::vector<T> bucketVals,
 	int nBuckets)
 {
 	std::vector<int64_t> retval(nBuckets, 0);
 
 	T val;
-	for (const Index& index : samples) {
+	for (const helper::Index& index : samples) {
 		band.p_band->RasterIO(GF_Read, index.x, index.y, 1, 1, &val, 1, 1, band.type, 0, 0);
 		for (int i = nBuckets - 1; i >= 0; i--) {
 			if (bucketVals[i] <= val) {
@@ -172,13 +173,14 @@ sampleDistribution(
 }
 
 template <typename T>
+void 
 calculateDist(
 	helper::RasterBandMetaData& band,
-	std::vector<Index>& sampled,
+	std::vector<helper::Index>& sampled,
 	int height,
 	int width,
 	int nBuckets,
-	std::unordered_map<std::string, std::pair<std::vector<double>, std::vector<int>>>& retval,
+	std::unordered_map<std::string, std::pair<std::vector<double>, std::vector<int64_t>>>& retval,
 	int nThreads)
 {
 	nThreads = 1; //for now testing
@@ -186,25 +188,25 @@ calculateDist(
 	//determine number of chunks and chunks size for each thread. A chunk is a group of blocks.
 	int yBlocks = (height + band.yBlockSize - 1) / band.yBlockSize;
 	int chunksize = yBlocks / nThreads;
-	int nChunks = (yBlocks + chunksize - 1) / chunkSize;
+	int nChunks = (yBlocks + chunksize - 1) / chunksize;
 	
 	//determine min and max values in the raster
 	std::vector<T> mins;
 	std::vector<T> maxs;
 	for (int chunk = 0; chunk < nChunks; chunk++) {
-		int yStartBlock = chunk * chunkSize;
-		int yEndBlock = std::min(yStartBlock + chunkSize, yBlocks);
+		int yStartBlock = chunk * chunksize;
+		int yEndBlock = std::min(yStartBlock + chunksize, yBlocks);
 
 		T min, max;
-		FindMinMax<T>(band, width, yStartBlock, yEndBlock, min, max);
+		findMinMax<T>(band, width, yStartBlock, yEndBlock, min, max);
 
 		//acquire mutex for adjusting these
 		mins.push_back(min);
 		maxs.push_back(max);
 		//release mutex for adjusting these
 	}
-	T min = std::min_element(mins.begin(), mins.end());
-	T max = std::max_element(maxs.begin(), maxs.end());
+	T min = *std::min_element(mins.begin(), mins.end());
+	T max = *std::max_element(maxs.begin(), maxs.end());
 
 	//call the setBuckets function which sets the vector<double> buckets to return to the user,
 	//and the vector<T> buckets to use to create the distribution. There is a seperate vector<T>
@@ -229,19 +231,20 @@ calculateDist(
 		//release mutex for adjusting these	
 	}
 
-	std::string name = band.p_band->GetDescription();
-	retval.emplace("population_" + name, {dbuckets, totalCounts});
+	std::string key = std::string("population_") + std::string(band.p_band->GetDescription());
+	retval.insert({key, {dbuckets, totalCounts}});
 
 	if (sampled.size() != 0) {
-		std::vector<T> sampleCounts = sampleDistribution<T>(band, sampled, tbuckets, nBuckets);
-		retval.emplace("sample_" + name, {dbuckets, sampleCounts});
+		std::vector<int64_t> sampleCounts = sampleDistribution<T>(band, sampled, tbuckets, nBuckets);
+		key = std::string("sample_") + std::string(band.p_band->GetDescription());
+		retval.insert({key, {dbuckets, sampleCounts}});
 	}
 }
 
 std::unordered_map<std::string, std::pair<std::vector<double>, std::vector<int64_t>>>
 dist(
 	raster::GDALRasterWrapper *p_raster,
-	int band,
+	int index,
 	vector::GDALVectorWrapper *p_vector,
 	std::string layer,
 	int nBuckets,
@@ -253,49 +256,51 @@ dist(
        	GDALInvGeoTransform(GT, IGT);	
 	
 	//get samples as vector if indices, if samples are given
-	std::vector<Index> sampled;
+	std::vector<helper::Index> sampled;
 	if (p_vector) {
 		OGRLayer *p_layer = p_vector->getDataset()->GetLayerByName(layer.c_str());
 		for (const auto& p_feature : *p_layer) {
 			OGRGeometry *p_geometry = p_feature->GetGeometryRef();
-			switch (wkbFlatter(p_geometry->getGeometryType())) {
+			switch (wkbFlatten(p_geometry->getGeometryType())) {
 				case OGRwkbGeometryType::wkbPoint: {
 					OGRPoint *p_point = p_geometry->toPoint();
-					double xCoord = p_point->GetX();
-					double yCoord = p_point->GetY();
-					sampled.push_back(Index(
-						IGT[0] + xCoord * IGT[1] + yCoord * IGT[2],
-						IGT[3] + xCoord * IGT[4] + yCoord * IGT[5]
+					double xCoord = p_point->getX();
+					double yCoord = p_point->getY();
+					sampled.push_back(helper::Index(
+						static_cast<int>(IGT[0] + xCoord * IGT[1] + yCoord * IGT[2]),
+						static_cast<int>(IGT[3] + xCoord * IGT[4] + yCoord * IGT[5])
 					));
 					break;
 				}
-				case OGRwkbGeometryType::skbMultiPoint: {
+				case OGRwkbGeometryType::wkbMultiPoint: {
 					for (const auto& p_point : *p_geometry->toMultiPoint()) {
-						double xCoord = p_point->GetX();
-						double yCoord = p_point->GetY();
-						sampled.push_back(Index(
-							IGT[0] + xCoord * IGT[1] + yCoord * IGT[2],
-							IGT[3] + xCoord * IGT[4] + yCoord * IGT[5]
+						double xCoord = p_point->getX();
+						double yCoord = p_point->getY();
+						sampled.push_back(helper::Index(
+							static_cast<int>(IGT[0] + xCoord * IGT[1] + yCoord * IGT[2]),
+							static_cast<int>(IGT[3] + xCoord * IGT[4] + yCoord * IGT[5])
 						));
 					}
 					break;
 				}
-				case default:
+				default:
 					throw std::runtime_error("encountered a geometry which was not a Point MultiPoint.");
 			}
 		}
 	}
 
-	std::pair<std::vector<double>, std::vector<int64_t>> retval;
-
-	int index = bandIndexes[i];
-	band.p_band = p_raster->GetRasterBand(index);
+	helper::RasterBandMetaData band;
+	band.p_band = p_raster->getRasterBand(index);
 	band.type = p_raster->getRasterBandType(index);
 	band.size = p_raster->getRasterBandTypeSize(index);
 	band.p_mutex = &datasetMutex;
 	band.nan = band.p_band->GetNoDataValue();
 	band.p_band->GetBlockSize(&band.xBlockSize, &band.yBlockSize);
-		
+	
+	int height = p_raster->getHeight();
+	int width = p_raster->getWidth();
+
+	std::unordered_map<std::string, std::pair<std::vector<double>, std::vector<int64_t>>> retval;
 	switch (band.type) {
 		case GDT_Int8: 
 			calculateDist<int8_t>(band, sampled, height, width, nBuckets, retval, nThreads);
