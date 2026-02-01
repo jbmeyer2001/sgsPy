8137a727d1f2263c955ea82cfb7a5d0aab44d4e6
jbmeyer2001 <jbmeyerii@gmail.com>
2026-01-31 22:04:34 -0800

    fix deploy for manylinux_2_28

--- scripts/build_dist.sh
+++
@@ -1,9 +0,0 @@
-#update git submodule
-git submodule update --init --recursive
-
-#set pkgconfig environment variables
-export PKG_CONFIG="${PWD}/sgspy/extern/vcpkg/installed/x64-linux/tools/pkgconf/pkgconf"
-export PKG_CONFIG_PATH="${PWD}/sgspy/extern/vcpkg/installed/x64-linux/lib/pkgconfig"
-
-python -m cibuildwheel --output-dir dist
-
